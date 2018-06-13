772307efa3345575c4a6bc3766f88e10e437ea85
XbaiAmy <1621810784@qq.com>
2018-06-13 21:25:00 +0800

    添加了一些页面的路由规则

--- wangyi_snail_reading/src/router/index.js
+++ wangyi_snail_reading/src/router/index.js
@@ -8,32 +8,56 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/home'
+      redirect: '/mine'
     },
-    {
-      path: '/home',
-      name: 'home',
-      component: resolve => require(['../components/Home.vue'],resolve)
-    },
+    // {
+    //   path: '/home',
+    //   name: 'home',
+    //   component: resolve => require(['../components/Home.vue'],resolve)
+    // },
     {
         path: '/read',
         name: 'read',
-        component: resolve => require(['../components/Read.vue'],resolve)
+        component: resolve => require(['../components/Home.vue'],resolve)
     },
     {
         path: '/catagory',
         name: 'catagory',
-        component: resolve => require(['../components/Catagory.vue'],reslove)
+        component: resolve => require(['../components/Home.vue'],resolve)
     },
     {
         path: '/desk',
         name: 'desk',
-        component: resolve => require(['../components/Desk.vue'],reslove)
+        component: resolve => require(['../components/Home.vue'],resolve)
     },
     {
       path: '/mine',
       name: 'mine',
-      component: resolve => require(['../components/Mine.vue'],resolve)
+      component: resolve => require(['../components/Home.vue'],resolve),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: resolve => require(['../components/Login.vue'], resolve),
+      meta: { title: '登录页面'}
+    },
+    {
+      path: '/comment',
+      name: 'comment',
+      component: resolve => require(['../components/Comment.vue'], resolve),
+      meta: { title: '写书评'}
+    },
+    {
+      path: '/article',
+      name: 'article',
+      component: resolve => require(['../components/Article.vue'], resolve),
+      meta: { title: '领读文章详情页'}
+    },
+    {
+      path: '/stock',
+      name: 'stock',
+      component: resolve => require(['../components/Stock.vue'], resolve),
+      meta: { title: '分类详情页'}
     },
   ]
 })
