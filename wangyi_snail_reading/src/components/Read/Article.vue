02bbcfab20e116f62bc139e28762ec581d69198e
XbaiAmy <1621810784@qq.com>
2018-06-15 22:51:21 +0800

    完善了领读页面的功能

---
+++ wangyi_snail_reading/src/components/Read/Article.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="a">
+        <mt-header title="书评正文" fixed class="header">
+            <router-link to="/" slot="left">
+                <mt-button icon="back"></mt-button>
+            </router-link>
+            <mt-button slot="right" style="color: #ba977a; font-size: 13px;">只看1本书</mt-button>
+        </mt-header>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.header {
+    background-color: #fff;
+    color: #333333;
+    border-bottom: 1px solid #ededed;
+    font-weight: bold;
+    text-decoration: none; 
+}
+</style>
---
+++ wangyi_snail_reading/src/components/Read/Comment.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="a">
+        <mt-header title="书评正文" fixed class="header">
+            <router-link to="/" slot="left">
+                <mt-button icon="back">返回</mt-button>
+            </router-link>
+            <mt-button slot="right">只看1本书</mt-button>
+        </mt-header>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.header {
+    background-color: #fff;
+    color: #333333;
+    border-bottom: 1px solid #ededed;
+    font-weight: bold;
+    text-decoration: none; 
+    font-size: 18px;
+}
+</style>
